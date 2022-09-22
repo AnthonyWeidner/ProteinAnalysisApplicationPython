b7f15d88d50fb6aa10667c03c955c4435102a5b3
Maggie <103873589+maggiexqc@users.noreply.github.com>
2022-09-22 16:12:16 -0700

    Successfully read third column of Excel data

--- main.py
+++ main.py
@@ -4,20 +4,7 @@ dataFrame = openpyxl.load_workbook("/Users/maggiechen/Downloads/SpNdirDIA_QizhiS
 
 dataFrameReader = dataFrame.active
 
-for row in range(0, 3):
-    for col in dataFrameReader.iter_cols(1, dataFrameReader.max_column):
+for row in range(0, dataFrameReader.max_row):
+    for col in dataFrameReader.iter_cols(3,3):
         print(col[row].value)
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
