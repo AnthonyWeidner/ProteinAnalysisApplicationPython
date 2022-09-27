9c9aa7882fc0f4f1a0a7b45f26adb770a29a72a2
Maggie <103873589+maggiexqc@users.noreply.github.com>
2022-09-27 16:11:41 -0700

    Sort all data entries and remove any nonnumerical values, store sample from each column into nested list, print number of valid proteins

--- main.py
+++ main.py
@@ -5,9 +5,26 @@ dataFrame = openpyxl.load_workbook("/Users/maggiechen/Downloads/SpNdirDIA_QizhiS
 dataFrameReader = dataFrame.active
 arr = []
 
+sampleName = ""
+flag = True
+counter = 0
 
-for row in dataFrameReader.iter_rows(0, dataFrameReader.max_row):
-    arr.append(row[4].value)
+for col in range(5, dataFrameReader.max_column):
+    arr.append([])
+    for row in dataFrameReader.iter_rows(0, dataFrameReader.max_row):
+        if flag:
+            sampleName = row[col].value
+            flag = False
+
+        value = row[col].value
+        if not isinstance(value, str):
+            arr[counter].append(value)
+
+    arr[counter].sort()
+    #print(arr)
+    print("Number of proteins with valid data in", sampleName, "=", len(arr[counter]))
+    counter += 1
+    flag = True
 
 arr.sort()
 print(arr)
