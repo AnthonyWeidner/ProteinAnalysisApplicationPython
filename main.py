90e84c9d3cce2376c3ddb3c890ac8d5604939804
AnthonyWeidner <alweidner@ucdavis.edu>
2022-12-17 20:17:06 -0800

    Add comments for each code/function block

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import statistics
 from matplotlib import pyplot as plt
 
 
+# @params:
+# choice: whether user wants to display graphical representation of results or not
+# protein_name: name of protein
+# desired_p_val: the value that the user entered for the desired p-value (i.e. 0.05, 0.01)
+# mu: population mean
 def pvalue_101(choice, protein_name, desired_p_val, mu, sigma, samp_size, samp_mean=0, deltam=0):
     np.random.seed(1234)
     s1 = np.random.normal(mu, sigma, samp_size)  # simulate random normal distribution
@@ -34,6 +39,8 @@ def pvalue_101(choice, protein_name, desired_p_val, mu, sigma, samp_size, samp_m
     return outliers
 
 
+# Set-up program
+# File selection
 root = tkinter.Tk()
 root.withdraw
 fileName1 = tkinter.filedialog.askopenfilename()
@@ -41,8 +48,11 @@ fileName1 = tkinter.filedialog.askopenfilename()
 dataFrame = openpyxl.load_workbook(fileName1)
 
 dataFrameReader = dataFrame.active
+
+# Creating the data structures
 arr = [] # goes down each column
 sampleData = {} # goes across each row
+
 
 sampleName = ""
 flag = True
@@ -67,6 +77,8 @@ for col in range(5, dataFrameReader.max_column):
 rowCounter = 0
 
 
+# Set-up control values
+# Goal: parse through just the control column and store data in dictionary
 controlDataMap = {}
 for row in dataFrameReader.iter_rows(2, dataFrameReader.max_row):
     proteinName = row[2].value
@@ -78,6 +90,8 @@ for row in dataFrameReader.iter_rows(2, dataFrameReader.max_row):
 
 
 # get data from left --> right
+# key = proteinName
+# value = list of data points
 for row in dataFrameReader.iter_rows(2, dataFrameReader.max_row):
     proteinName = row[2].value
     sampleData[proteinName] = []
@@ -94,6 +108,7 @@ desired_p_value = (float)(input("Enter the desired p value: "))
 graphs = input("Would you like to see a graphical display of the statistics for statistically significant proteins? (y/n)")
 
 for key,val in sampleData.items():
+    # If entire data set is invalid (i.e. "Filtered) then ignore this sample
     if len(val) <= 0:
         continue
 
