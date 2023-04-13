20358da0f7fd3efbf7d8b57f3e50a4b0025435e2
AnthonyWeidner <alweidner@ucdavis.edu>
2023-04-12 21:33:06 -0700

    PostgreSQL database integration SQL Queries

---
+++ postgreSQL.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+con = psycopg2.connect(
+    database="TestingDataBase",
+    user="postgres",
+    password="axdzm3zp9",
+    host="localhost",
+    port=5432
+)
+
+cursor_obj = con.cursor()
+#cursor_obj.execute("CREATE TABLE person (id BIGINT NOT NULL PRIMARY KEY, first_name VARCHAR(50))")
+#cursor_obj.execute("INSERT INTO person (id, first_name) VALUES ('1', 'Sally')")
+#cursor_obj.execute("SELECT * FROM person")
+
+#result = cursor_obj.fetchall()
+
+#cursor_obj.execute("INSERT INTO PERSON(id, first_name) VALUES (2, 'Bob')")
+
+#insert_script = "INSERT INTO person (id, first_name) VALUES (3, 'Hi')"
+#cursor_obj.execute(insert_script)
+
+#create_table_script = "CREATE TABLE car (id BIGSERIAL NOT NULL PRIMARY KEY, model VARCHAR(30))"
+#cursor_obj.execute(create_table_script)
+
+create_car_script = "INSERT INTO car (model) VALUES ('Honda')"
+cursor_obj.execute(create_car_script)
+
+con.commit()
+
+cursor_obj.close()
+con.close()
+
+#print(result)
